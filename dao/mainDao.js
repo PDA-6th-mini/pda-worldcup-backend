43a2baa699eb5303682b582920e7ee927526ee12
임채연 <dlacodus0407@gmail.com>
2025-06-23 15:16:21 +0900

    init

--- dao/mainDao.js
+++ dao/mainDao.js
@@ -2,7 +2,6 @@ const mainDao = {
     selectProblems: async (connection) => {
         const query = `SELECT * FROM PROBLEM`;
         const selectedRow = await connection.query(query);
-        console.log(selectedRow)
         return selectedRow;
     },
 
