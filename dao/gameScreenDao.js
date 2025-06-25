aba0245efbe1003cf223401ae60fe3946bb562f7
Jihwan Lee <wlghks0508@naver.com>
2025-06-25 16:33:54 +0900

    #6 게임 파트 Problem, Img 조회 API

---
+++ dao/gameScreenDao.js
@@ -0,0 +1,19 @@
+const gameScreenDao = {
+  selectImgs: async (connection, problemId) => {
+    const query = `SELECT 
+  p.problem_id,
+  p.name,
+  p.description,
+  i.img_id,
+  i.img_name,
+  i.img_url
+FROM problem p
+JOIN img i ON p.problem_id = i.problem_id
+WHERE p.problem_id = 1;
+`;
+    const rows = await connection.query(query, [problemId]);
+    return rows;
+  },
+};
+
+module.exports = gameScreenDao;
