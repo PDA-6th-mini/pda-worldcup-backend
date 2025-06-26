13d79a91b0aeaf80c26924439db983b3d5003d9b
임채연 <dlacodus0407@gmail.com>
2025-06-26 11:35:31 +0900

    refactor : main page 데이터 받아오는 쿼리 수정

--- services/mainService.js
+++ services/mainService.js
@@ -2,10 +2,16 @@ const pool = require('../common/database');
 const { selectProblems } = require('../dao/mainDao');
 
 const mainService = {
+    /**
+     * 목요일 TODO
+     * 1. 예외처리를 해야되는데 : arr가 0이면.... image data를 다시 조회해서 그 위에있는거 상위 2개 그냥 가져와서 response 준다.
+     */
+
     getProblems: async () => {
         const conn = await pool.getConnection();
         const rows = await selectProblems(conn);
         const parsed = _parseData(rows);
+
         return parsed;
     },
 }
@@ -37,7 +43,7 @@ const _parseData = (rows) => {
         });
     }
 
-    return problemData;
+    return Object.values(problemData);
 };
 
 module.exports = mainService;
