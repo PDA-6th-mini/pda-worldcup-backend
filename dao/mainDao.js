a4208ae0eb648aa9e38fc7a32ac940280945de9c
임채연 <dlacodus0407@gmail.com>
2025-06-30 22:29:33 +0900

    refactor : 페이지네이션 쿼리 수정
    - 쿼리를 간략하게 수정

--- dao/mainDao.js
+++ dao/mainDao.js
@@ -2,37 +2,25 @@
 const mainDao = {
   selectProblems: async (connection, cursor) => {
     const query = `
-    WITH problem_cursor AS (
-      SELECT problem_id
+    SELECT 
+    p.problem_id,
+    p.name,
+    p.description,
+    i.img_id,
+    i.img_url,
+    COUNT(im.img_id) AS meta_count
+    FROM (
+      SELECT *
       FROM problem
       WHERE problem_id < ?
       ORDER BY problem_id DESC
       LIMIT 8
-    ),
-    img_with_meta_count AS (
-      SELECT 
-        i.img_id,
-        i.problem_id,
-        i.img_url,
-        COUNT(im.img_id) AS meta_count
-      FROM img i
-      LEFT JOIN image_meta im ON i.img_id = im.img_id
-      GROUP BY i.img_id, i.problem_id, i.img_url
-    )
-    SELECT 
-      p.problem_id,
-      p.name,
-      p.description,
-      i.img_id,
-      i.img_url,
-      imc.meta_count
-    FROM problem_cursor pc
-    JOIN problem p ON p.problem_id = pc.problem_id
-    JOIN img_with_meta_count imc ON imc.problem_id = p.problem_id
-    JOIN img i ON i.img_id = imc.img_id
-    ORDER BY p.problem_id DESC, imc.meta_count DESC, i.img_id DESC;
+    ) AS p
+    JOIN img i ON i.problem_id = p.problem_id
+    LEFT JOIN image_meta im ON im.img_id = i.img_id
+    GROUP BY i.img_id, p.problem_id, p.name, p.description, i.img_url
+    ORDER BY p.problem_id DESC, meta_count DESC, i.img_id DESC;
     `
-
     const params = [
       cursor?.cursor_problem_id ?? Number.MAX_SAFE_INTEGER
     ];
