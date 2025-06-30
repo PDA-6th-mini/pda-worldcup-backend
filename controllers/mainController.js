cefc7d465cae30a5ce35ebbccfeeee6bc5873608
임채연 <dlacodus0407@gmail.com>
2025-06-30 09:57:46 +0900

    feat : 백엔드 cursor 기반 페이지네이션 구현
    - offset을 사용하지 않고, no-offset(cursor)을 이용해서 페이지네이션 구현

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,12 +1,30 @@
-const { successResponse, failResponse } = require("../common/Response");
-const { readSuccess } = require("../common/responseMessages")
-const mainService = require("../services/mainService");
+const { successResponse, failResponse } = require('../common/Response');
+const { readSuccess } = require('../common/responseMessages');
+const mainService = require('../services/mainService');
 
 const main = {
-    mainPage: async (req, res) => {
-        const problems = await mainService.getProblems();
-        res.json(successResponse(readSuccess.status, readSuccess.message, problems));
-    }
-}
+	mainPage: async (req, res) => {
+		const {
+			cursor_total_count,
+			cursor_count,
+			cursor_img_id,
+		} = req.query;
+
+		// 숫자형으로 파싱
+		const cursor = cursor_total_count
+			? {
+					cursor_total_count: Number(cursor_total_count),
+					cursor_count: Number(cursor_count),
+					cursor_img_id: Number(cursor_img_id),
+			  }
+			: null;
+
+
+		const problems = await mainService.getProblems(cursor);
+        console.log(problems)
+        
+		res.json(successResponse(readSuccess.status, readSuccess.message, problems));
+	},
+};
 
 module.exports = main;
--- dao/mainDao.js
+++ dao/mainDao.js
@@ -1,32 +1,68 @@
+// mainDao.js
 const mainDao = {
-    selectProblems: async (connection) => {
+    selectProblems: async (connection, cursor) => {
         const query = `
-SELECT
-    p.problem_id, 
-    p.name,
-    p.description, 
-    i.img_id,
-    i.img_url, 
-    COUNT(im.img_id) AS count,
-    ptc.TOTAL_COUNT
-    FROM img i
-    LEFT JOIN image_meta im ON i.img_id = im.img_id
-    JOIN problem p ON i.problem_id = p.problem_id
-    LEFT JOIN (
+        WITH top_problems AS (
+          SELECT 
+            p.problem_id,
+            ptc.total_count,
+            COUNT(im.img_id) AS count,
+            MAX(i.img_id) AS max_img_id
+          FROM img i
+          JOIN problem p ON i.problem_id = p.problem_id
+          LEFT JOIN image_meta im ON i.img_id = im.img_id
+          LEFT JOIN (
+            SELECT 
+              i.problem_id, 
+              COUNT(im.img_id) AS total_count
+            FROM img i
+            LEFT JOIN image_meta im ON i.img_id = im.img_id
+            GROUP BY i.problem_id
+          ) AS ptc ON p.problem_id = ptc.problem_id
+          GROUP BY p.problem_id, ptc.total_count
+          HAVING 
+            (ptc.total_count < ?)
+            OR (ptc.total_count = ? AND COUNT(im.img_id) < ?)
+            OR (ptc.total_count = ? AND COUNT(im.img_id) = ? AND MAX(i.img_id) < ?)
+          ORDER BY ptc.total_count DESC, COUNT(im.img_id) DESC, MAX(i.img_id) DESC
+          LIMIT 8
+        )
+            
         SELECT 
+          p.problem_id,
+          p.name,
+          p.description,
+          i.img_id,
+          i.img_url,
+          COUNT(im.img_id) AS count,
+          ptc.total_count
+        FROM top_problems tp
+        JOIN problem p ON p.problem_id = tp.problem_id
+        JOIN img i ON i.problem_id = p.problem_id
+        LEFT JOIN image_meta im ON im.img_id = i.img_id
+        LEFT JOIN (
+          SELECT 
             i.problem_id, 
-            COUNT(i.img_id) AS TOTAL_COUNT
-        FROM img i
-        JOIN image_meta im ON i.img_id = im.img_id
-        GROUP BY i.problem_id
+            COUNT(im.img_id) AS total_count
+          FROM img i
+          LEFT JOIN image_meta im ON i.img_id = im.img_id
+          GROUP BY i.problem_id
         ) AS ptc ON p.problem_id = ptc.problem_id
-        GROUP BY p.problem_id, p.name, p.description, i.img_id, i.img_url, ptc.TOTAL_COUNT
-        ORDER BY ptc.TOTAL_COUNT DESC, count DESC;`
+        GROUP BY i.img_id, p.problem_id, p.name, p.description, ptc.total_count
+        ORDER BY ptc.total_count DESC, count DESC, i.img_id DESC;`
 
-        const selectedRow = await connection.query(query);
-        return selectedRow;
+        const params = [
+            cursor?.cursor_total_count ?? Number.MAX_SAFE_INTEGER,
+            cursor?.cursor_total_count ?? Number.MAX_SAFE_INTEGER,
+            cursor?.cursor_count ?? Number.MAX_SAFE_INTEGER,
+            cursor?.cursor_total_count ?? Number.MAX_SAFE_INTEGER,
+            cursor?.cursor_count ?? Number.MAX_SAFE_INTEGER,
+            cursor?.cursor_img_id ?? Number.MAX_SAFE_INTEGER,
+        ];
+
+        const rows = await connection.query(query, params);
+        return rows;
     },
-
-}
+};
 
 module.exports = mainDao;
